ca9b3eea576fda07017b1801afaf0a6efbfbd1f7
sayedex <sayedadmod@gmail.com>
2024-05-03 20:08:56 +0600

    add padding for swap panel

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -15,7 +15,7 @@ function Swap({}: Props) {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center  items-center sidenav-content relative pb-24 ">
+    <div className="min-h-screen pt-20 flex flex-col justify-center  items-center sidenav-content relative pb-24 ">
       <div className=" m-3 swap_main z-10 relative    w-[95vw] mx-auto  md:max-w-[480px] py-2 px-4 rounded-3xl bg-gradient-to-b from-[#185a2b1a] to-neutral-800 ">
         {/* tab */}
         <div className="flex flex-row items-center justify-between gap-x-2 font-normal w-full">
