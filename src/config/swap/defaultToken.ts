80d2ea982a4711af7f7f9ce79c198a3a5955b368
sayedex <sayedadmod@gmail.com>
2024-05-04 20:11:30 +0600

    add bnb as input token

--- src/config/swap/defaultToken.ts
+++ src/config/swap/defaultToken.ts
@@ -5,26 +5,26 @@ const tokenListFromSDK = {
   56: BSCToken.tokens,
 };
 
-const defaultToken: Record<number, Record<Field, TOKEN>> = {
+
+  const defaultToken: Record<number, Record<Field, TOKEN>> = {
     56: {
       [Field.INPUT]: {
+        "name": "Binance smart chain",
+        "address": "0x000000000000000000000000000000000000dEaD",
+        "symbol": "BNB",
+        "decimals": 18,
+        "chainId": 56,
+        "logoURI": "https://assets-cdn.trustwallet.com/blockchains/smartchain/assets/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c/logo.png",
+        "isNative":true
+      },
+      [Field.OUTPUT]: {
         name: "Agatech",
         address: "0xb427e47e8fDD678278d2A91EEaC014ffcDDaF029",
         symbol: "AGATA",
         decimals: 18,
         chainId: 56,
         logoURI: "https://assets.coingecko.com/coins/images/35835/thumb/IMG_20240229_111252_206.jpg?1709888815",
-      },
-      [Field.OUTPUT]: {
-        name: "Binance Pegged USDT",
-        address: "0x55d398326f99059fF775485246999027B3197955",
-        symbol: "USDT",
-        decimals: 18,
-        chainId: 56,
-        logoURI: "https://tokens.pancakeswap.finance/images/0x55d398326f99059fF775485246999027B3197955.png"
-
-  
-      },
+      }
     },
   };
 
