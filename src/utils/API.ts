9ab8b10db2f0165d8af8084823303bcfa3be98a5
sayedex <sayedadmod@gmail.com>
2024-05-18 08:39:21 +0600

    fixed api

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //http://31.220.53.102:5000/
 //http://localhost:5000/api/
 
-const apiUrl = process.env.NODE_ENV === 'production' ? "https://agatech.com/api": 'http://localhost:3000/api';
+const apiUrl = process.env.NODE_ENV === 'production' ? "https://agatech.io/api": 'http://localhost:3000/api';
 
 //https://api.cipherem.xyz/api
 const newRequest = axios.create({
